bb721a59b1d88062bdc5906b0fbfd099bd63d1b8
karapuz1337 <belanoff228@gmail.com>
2025-09-10 22:21:37 +0300

    feat: hw4.1 done - JavaScript basics
    Added 4 task files inside the jsBasics folder with the completed assignments for homework 4.1

---
+++ jsBasics/task3.js
@@ -0,0 +1,5 @@
+const age = 17
+const isAdult = age >= 18
+
+console.log("Person by the age of " + age + " is an adult? " + isAdult.toString().toUpperCase())
+// Output: Person by the age of 17 is an adult? FALSE
---
+++ jsBasics/task4.js
@@ -0,0 +1,23 @@
+// 4.1
+const circleRadius = 5.5
+const circleArea = Math.PI * Math.pow(circleRadius, 2)
+console.log(`The area of a circle is ${circleArea.toFixed(2)}`)
+
+// 4.2
+const rectLength = 6.2
+const rectWidth = 4
+const rectArea = rectLength * rectWidth
+console.log(`The area of a rectangle is ${rectArea.toFixed(2)}`)
+
+// 4.3
+const cylinderRadius = 12.31
+const cylinderHeight = 26.2
+const cylinderVolume = Math.PI * Math.pow(cylinderRadius, 2) * cylinderHeight
+console.log(`The volume of a cylinder is ${cylinderVolume.toFixed(2)}`)
+
+/*
+Output:
+The area of a circle is 95.03
+The area of a rectangle is 24.80
+The volume of a cylinder is 12472.90
+ */
