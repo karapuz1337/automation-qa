7a942b4ed8574d6992c99d1400a63d9350e9e9d1
karapuz1337 <belanoff228@gmail.com>
2025-09-29 21:06:33 +0300

    fix: replace indexOf func with provided param to map method in task2.js

--- 8.1_arrays/task2.js
+++ 8.1_arrays/task2.js
@@ -2,6 +2,6 @@
 const sourceNumbers = [1, 2, 3, 4, 5]
 
 // create a multiplied array that takes each number and multiplies it to its index in the source array
-const multipliedNumbers = sourceNumbers.map(num => num * sourceNumbers.indexOf(num))
+const multipliedNumbers = sourceNumbers.map(((num, index) => num * index))
 
 console.log(multipliedNumbers) // [ 0, 2, 6, 12, 20 ]
