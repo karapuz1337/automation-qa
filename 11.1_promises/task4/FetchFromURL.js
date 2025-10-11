0625ddee7e3a185128fa46ba5bf1d6ac60e36f99
karapuz1337 <belanoff228@gmail.com>
2025-10-11 22:03:20 +0300

    feat: hw11.1 completed - Promises
    Added the 11.1_promises folder with 4 completed tasks inside, including the optional task!

---
+++ 11.1_promises/task4/FetchFromURL.js
@@ -0,0 +1,48 @@
+// Create a Fetch class with async methods
+export default class FetchFromURL {
+    constructor(url) {
+        this.url = url;
+    }
+
+    // Method to fetch all the objects
+    // Takes the target to fetch (todo or user)
+    async fetchAll(target) {
+        // Check if target param is correctly provided
+        if (target !== "todo" && target !== "user") {
+            throw new Error("Please provide the target. Available targets: todo, user");
+        }
+
+        return fetch(`${this.url}${target}/ `)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    else {
+                        throw new Error (`Failed to fetch data: ${response.status} ${response.statusText}`);
+                    }
+                });
+    }
+
+    // Method to fetch object by id
+    async fetchById(target, id) {
+        // Check if target param is correctly provided
+        if (target !== "todo" || target !== "user") {
+            throw new Error("Please provide the target. Available targets: todo, user");
+        }
+
+        // Check if id param is correctly provided
+        if (!id || typeof id !== "number") {
+            throw new Error("Please provide the 'id' parameter as a number");
+        }
+
+        return fetch(`${this.url}${target}/${id} `)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    throw new Error (`Failed to fetch data: ${response.status} ${response.statusText}`)
+                }
+            });
+    }
+}
